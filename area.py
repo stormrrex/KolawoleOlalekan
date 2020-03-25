54e626b781929fac4612d832cef4b8b9c336202f
stormrrex <61963835+stormrrex@users.noreply.github.com>
2020-03-24 19:02:50 -1100

    Add files via upload

---
+++ area.py
@@ -0,0 +1,7 @@
+print("Please Enter the radius of the Circle")
+
+radius= int(input()) #get user input and converts to String
+
+area= (22/7)*radius*radius
+
+print("The Area of the Circle is ",area)
